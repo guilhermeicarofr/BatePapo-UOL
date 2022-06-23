0d5ce7be745dc180845f9b90308786272eb9a3e6
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-06-23 02:20:01 -0300

    implementado o get do feed de mensagens com polling de 3s e identificacao de categoria da mensagem e rolagem automática, correcoes e melhorias de layout. Correção no overflow de uma mensagem maior que a div necessária. Mensagens reservadas ainda não implementadas.

---
+++ script.js
@@ -0,0 +1,63 @@
+function refreshChat() {
+    console.log("refresh");
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(renderChat);
+}
+
+function renderChat(get) {
+    const chat = get.data;
+    document.querySelector('.chat').innerHTML = '';
+    for (let i=0 ; i<chat.length ; i++) {
+
+        switch (chat[i].type) {
+            case "message":
+                document.querySelector('.chat').innerHTML += `
+                    <div class="msg">
+                        &nbsp;
+                        <span class="time">(${chat[i].time})</span>
+                        &nbsp;
+                        <span class="user">${chat[i].from}</span>
+                        &nbsp;para&nbsp;
+                        <span class="target">${chat[i].to}</span>
+                        :&nbsp;
+                        <span class="text">${chat[i].text}</span>
+                    </div>
+                `;
+                break;
+
+            case "status":
+                document.querySelector('.chat').innerHTML += `
+                    <div class="status">
+                        &nbsp;
+                        <span class="time">(${chat[i].time})</span>
+                        &nbsp;
+                        <span class="user">${chat[i].from}</span>
+                        &nbsp;
+                        <span class="text">${chat[i].text}</span>
+                    </div>
+                `;
+                break;
+
+            case "private-message":
+                document.querySelector('.chat').innerHTML += `
+                    <div class="pvt-msg">
+                        &nbsp;
+                        <span class="time">(${chat[i].time})</span>
+                        &nbsp;
+                        <span class="user">${chat[i].from}</span>
+                        &nbsp;reservadamente&nbsp;para&nbsp;
+                        <span class="target">${chat[i].to}</span>
+                        :&nbsp;
+                        <span class="text">${chat[i].text}</span>
+                    </div>
+                `;
+                break;
+        }
+
+        document.querySelector('.chat div:last-of-type').scrollIntoView();
+
+    }
+}
+
+
+setInterval(refreshChat, 3000);
